6fd1f8d22584dc5e55b64e7d69e16dfa33ddbd05
PC hp marisol <marmil.cg@gmail.com>
2025-01-24 13:59:00 -0600

    Viernes 1 pm

---
+++ Semana 1/Trabajosalon1.py
@@ -0,0 +1,35 @@
+print("Actividades salón")
+'''''
+#nombre = input ()
+#print ("Tu nombre es:", nombre)
+int ()
+float()
+str()
+
+numero=int(float(input("Ingrese un numero")))
+
+
+edad = -1
+añoactual = 2025
+añonacimiento = input ("Ingrese el año de nacimiento:")
+edad = añoactual - añonacimiento
+print ("Tienes" , edad, "años")
+'''''
+#Actividad 2
+
+
+n1 = int(input("Ingrese el primer número "))
+n2 = int(input ("ingrese el segundo número "))
+print ("suma = ",n1 + n2)
+print("resta =", n1-n2)
+print("multiplicación=", n1*n2)
+print("división=", n1/n2)
+
+if condicional:
+    comandos
+elif condicional:
+
+    else
+
+#Actividad 3 
+# hacer el factorial 
