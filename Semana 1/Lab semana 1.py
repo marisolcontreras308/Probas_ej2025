6760f20d2cffca439654606cf75eee5918625c89
PC hp marisol <marmil.cg@gmail.com>
2025-01-26 18:50:29 -0600

    Lab semana 1

---
+++ Semana 1/Lab semana 1.py
@@ -0,0 +1,40 @@
+#Ejercicios
+print ("Ejercicio 1:¿Cuál es la función de una memoria secundaria en una computadora?")
+respuesta1= str("C) Almacenar información durante mucho tiempo,incluso después de ciclos de apagado y encendido")
+print (respuesta1)
+
+print("Ejercicio 2:¿Qué es un programa?")
+print("Es una secuencia de declaraciones o sentencias que han sido diseñadas para hacer algo.")
+      
+print("Ejercicio 3 ¿Cuál es la diferencia entre un compilador y un intérprete?")
+print("Un intérprete puede leer el código fuente tal como ha sido escrito, interpreta las instrucciones sobre la marcha.En cambio el compilador necesita realizar un proceso para traducir el el código fuente de alto nivel a código máquina y después lo ejecuta")
+
+print ("Ejercicio 1:¿Cuál de los siguientes contiene código máquina?")
+respuesta4= str("A) El intérprete de Python")
+print (respuesta4)
+
+print("Ejercicio 5 ¿Qué está mal en este código?")
+print("La instrucción para impirmir correcta sería Print, además faltaron los paréntesis")
+#La instrucción correcta sería print("¡Hola mundo!")
+
+print ("Ejercicio 6:¿En qué lugar del computador queda almacenada una variable, como este caso X después de ejecutar la siguiente línea de python?")
+respuesta6= str("B) Memoria principal")
+print (respuesta6)
+
+print("Ejercicio 7 ¿Qué mostrará en la pantalla el siguiente programa?")
+# x=43
+# x=x+1
+# print("x")
+respuesta7= str("B) 44")
+print (respuesta7)
+
+print("Ejercicio 8: Explica los siguientes conceptos usando un ejemplo de una capacidad humana")
+CPU=str("El CPU :Es el cerebro, recibe instrucciones y las procesa, puede enviar señales a todo el cuerpo.")
+Memoriapr=str ("Memoria principal:Es como la memoria a corto plazo que recuerda las cosas que necesitamos para vivir en el momento.")
+memoriasec= str("Memoria secundaria:La memoria a largo plazo, que almacena los recuerdos pasados aunque haya pasado mucho tiempo desde que sucedieron.")
+dispentrada= str("Los dispositivos de entrada son como los cinco sentidos, le hacen saber al cuerpo información del exterior.")
+dispsalida= str("Los dispositivos de salida serían como los síntomas del cuerpo, el calor, el hambre, el sueño que reflejan lo que pasa desde dentro.")
+print(Memoriapr, memoriasec, dispentrada, dispsalida)
+
+print("Ejercicio 9: ¿Cómo se corrige un error de sintáxis?")
+print("Detectando la línea dónde se cometío el error y asegurarse que la instrucción este correctamente escrita con los caracteres necesarios")
