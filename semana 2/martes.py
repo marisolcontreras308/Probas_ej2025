cb00116395549af70e0a7a72d0650d482b983832
PC hp marisol <marmil.cg@gmail.com>
2025-01-28 14:01:45 -0600

    martes 28

---
+++ semana 2/martes.py
@@ -0,0 +1,29 @@
+#tipos de variables
+str()
+int()
+float()
+
+list()
+dict
+set
+tuple
+
+#enumerate
+#range
+
+diasSeman= ('Lun','Mar','Mier','Jue','Vie')
+
+combinado=(1, 2.34,'Alzheimer',diasSeman)
+
+lista=list([1,2,3])*40
+#salen 1,2,3 40 veces
+print(type(lista))
+lista=[0]*40
+#salen 40 ceros
+print(type(lista))
+
+conjunto=set()
+conjunto={1,2,3}
+
+tupla=tuple()
+diccionario={'pi':3.14,'e':2.74}
