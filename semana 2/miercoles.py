4e152fa4757ce298ed5736a7e8e66403b572d1c4
PC hp marisol <marmil.cg@gmail.com>
2025-01-30 11:54:23 -0600

    miercoles 29

---
+++ semana 2/miercoles.py
@@ -0,0 +1,11 @@
+#   Tipos de datos estructurados compuestos
+meses=['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio',' Agosto','Septiembre','Octubre','Noviembre','Dicimebre']
+for mes in enumerate (meses,1):
+    print(mes)
+#le pones ,1 para que no empiece de 0
+
+for j in range(13):
+    print(j)
+
+for j in range(1,14,1):
+    print(j)
